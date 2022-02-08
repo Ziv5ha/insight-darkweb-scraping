1fdc5c00378c7db5330944609cdff48bb4b8ab07
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-02-08 19:40:06 +0200

    integrate SSE into react

---
+++ front/src/@types/types.d.ts
@@ -0,0 +1,13 @@
+declare interface Paste {
+  author: string;
+  title: string;
+  content: string;
+  date: Date;
+}
+declare interface AnalyticsType {
+  total: number;
+  porn: number;
+  dataleaks: number;
+  money: number;
+  other: number;
+}
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+const url = 'http://localhost:8080/';
 function App() {
-  return <div className='App'></div>;
+  const [data, setData] = useState<Paste[]>([]);
+  const [analytics, setAnalytics] = useState<AnalyticsType>({
+    total: 0,
+    porn: 0,
+    dataleaks: 0,
+    money: 0,
+    other: 0,
+  });
+  useEffect(() => {
+    let source = new EventSource(url);
+    source.onmessage = (e) => {
+      setData(JSON.parse(e.data).pastes);
+      setAnalytics(JSON.parse(e.data).analytics);
+    };
+    source.onerror = () => {
+      source.close();
+    };
+  }, []);
+
+  return (
+    <div className='App'>
+    </div>
+  );
 }
 
 export default App;
