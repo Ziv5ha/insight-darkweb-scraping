94219c765698235879c4c9dc107f3cc1104a0946
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-02-10 15:32:37 +0200

    added search bar with lodash

--- front/src/components/Feed.tsx
+++ front/src/components/Feed.tsx
@@ -1,14 +1,21 @@
 import React, { useContext, useState } from 'react';
 import Post from './Post';
+import SearchBar from './SearchBar';
 
 export default function Feed({ data }: { data: Paste[] }) {
+  const [localData, setLocalData] = useState<Paste[]>([]);
   const renderFeed = () => {
+    if (localData.length > 0)
+      return localData.map((entry) => (
+        <Post key={entry.date + entry.title} entry={entry} />
+      ));
     return data.map((entry) => (
       <Post key={entry.date + entry.title} entry={entry} />
     ));
   };
   return (
     <div>
+      <SearchBar setLocalData={setLocalData} data={data} />
       <div className='feed'>{renderFeed()}</div>
     </div>
   );
---
+++ front/src/components/SearchBar.tsx
@@ -0,0 +1,44 @@
+import React, { useContext, useRef } from 'react';
+import _ from 'lodash';
+
+export default function SearchBar({
+  setLocalData,
+  data,
+}: {
+  setLocalData: React.Dispatch<React.SetStateAction<Paste[]>>;
+  data: Paste[];
+}) {
+  const filterValues = useRef<HTMLInputElement>(null);
+  const filterLocally = () => {
+    if (filterValues.current) {
+      const filterBy = filterValues.current.value;
+      const filteredArr = data.filter(
+        (entry) =>
+          entry.title.toLowerCase().includes(filterBy.toLowerCase()) ||
+          entry.content.toLowerCase().includes(filterBy.toLowerCase()) ||
+          entry.author.toLowerCase().includes(filterBy.toLowerCase())
+      );
+      setLocalData(filteredArr.length ? filteredArr : []);
+      // errorMessage(filteredArr, filterBy);
+    }
+  };
+  // const errorMessage = (data, filterVal) => {
+  //   if (data.length === 0 && filterVal) {
+  //     document.querySelector('.searchError').textContent = 'Not Found';
+  //   } else {
+  //     document.querySelector('.searchError').textContent = '';
+  //   }
+  // };
+  const debouncedFilter = _.debounce(filterLocally, 300);
+  return (
+    <div>
+      <input
+        ref={filterValues}
+        onKeyDown={debouncedFilter}
+        className='searchBar'
+        placeholder='Search'
+      ></input>
+      <div className='searchError'></div>
+    </div>
+  );
+}
