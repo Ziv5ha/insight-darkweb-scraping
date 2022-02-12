357b6e2031108fab41822d92b3f6b3451f05b126
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-02-12 19:37:28 +0200

    fix SSE bug

--- controllers/getAllPastes.ts
+++ controllers/getAllPastes.ts
@@ -3,9 +3,13 @@ import { analysis } from '../helpers/analysis';
 import { traffic } from '../helpers/traffic';
 import { clients } from '../middlewares/clients';
 import PasteModel from '../mongo/model';
+import { scraper } from '../scraper';
+
+const strongholdUrl =
+  'http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all';
 
 export const retrieveData = async (
-  _req: Request,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
@@ -17,6 +21,12 @@ export const retrieveData = async (
     clients.forEach((client) =>
       client.write(`data: ${JSON.stringify(data)}\n\n`)
     );
+    // call on scrape and send the new message to all active clients
+    setTimeout(async () => {
+      await scraper(strongholdUrl, '#list > .row');
+      console.log('scrapped');
+      retrieveData(req, res, next);
+    }, 120000); //every 2 minutes
   } catch (error) {
     // TODO error handler
     res.status(400).send('oh no');
--- scraper.ts
+++ scraper.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { parse } from 'node-html-parser';
 import dataExtractor from './utils/extractData';
-import * as cron from 'node-cron';
-import PasteModel from './mongo/model';
-import { connection } from 'mongoose';
+// import * as cron from 'node-cron';
+// import PasteModel from './mongo/model';
+// import { connection } from 'mongoose';
 
 const strongholdUrl =
   'http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all';
 
-const scraper = async (url: string, divSelector: string) => {
+export const scraper = async (url: string, divSelector: string) => {
   try {
     const request = await axios.get(url, {
       proxy: { port: 8118, host: 'localhost' },
@@ -21,7 +21,7 @@ const scraper = async (url: string, divSelector: string) => {
   }
 };
 
-scraper(strongholdUrl, '#list > .row');
-cron.schedule('*/2 * * * *', () => {
-  scraper(strongholdUrl, '#list > .row');
-});
+// scraper(strongholdUrl, '#list > .row');
+// cron.schedule('*/2 * * * *', () => {
+//   scraper(strongholdUrl, '#list > .row');
+// });
